c240d2bc1f11226a85cd524956b384dcac4d881a
FrontMD <web@mockup.digital>
2024-09-07 17:09:07 +0300

    js in progress

--- src/blocks/modules/modals/modals.js
+++ src/blocks/modules/modals/modals.js
@@ -1,15 +1,3 @@
 const modals = new HystModal({
     linkAttributeName: "data-modal"
 });
-
-function openRestauranModal() {
-    const restaurantList = document.querySelector('[data-js="restaurantList"]')
-    
-    if(!restaurantList) return
-
-    restaurantList.addEventListener("click", e => {
-        if(e.target.closest('[data-js="openRestaurantBtn"]')) {
-            modals.open('[data-js="restaurantModal"]')
-        }
-    })
-}
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -3,5 +3,4 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     headerControllInit()
-    openRestauranModal()
 })
